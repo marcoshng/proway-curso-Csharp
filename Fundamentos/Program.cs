8854b08098efb7d244d5928964e8d96187c536f5
Marcos Henrique Giovanella <73296@PROWAY.TREINA>
2023-04-22 15:14:23 -0300

    Exemplos de Estrutura Condicional

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,21 @@
+﻿using Fundamentos.ApresentarDados;
+using Fundamentos.EntradaDados;
+using Fundamentos.EstruturaCondicional;
+
+//ApresentarDados01 executor = new ApresentarDados01();
+//executor.Executar();
+
+
+//EstruturaCondicionalAnd executor = new EstruturaCondicionalAnd();
+//executor.Executar();
+
+//EntradaDados01 executor2 = new EntradaDados01();
+//executor2.Executar();
+
+//executor.Executar();
+
+//EstruturaCondicionalOu executor = new EstruturaCondicionalOu();
+//executor.Executar();
+
+EstruturaCondicionalAvancada executor = new EstruturaCondicionalAvancada();
+executor.Executar();
